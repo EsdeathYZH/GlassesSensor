55d44635daa3d0524e00652d0f018d2589fdd214
EsdeathYZH <1162526220@qq.com>
2018-01-19 11:28:14 +0800

    Init all files.

---
+++ app/src/main/java/com/example/sensor/FileHelper.java
@@ -0,0 +1,94 @@
+package com.example.sensor;
+
+/**
+ * Created by Tony on 2017/12/16.
+ */
+
+import android.content.Context;
+
+import java.io.BufferedWriter;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+
+public class FileHelper {
+    public static final int MODE_LIGHT=0;
+    public static final int MODE_ACCELEROMETER=1;
+    public static final int MODE_MAGNETIC=2;
+    public static final int MODE_GYROSCOPE=3;
+    private Context mContext;
+    private BufferedWriter lightWriter;
+    private BufferedWriter accelerometerWriter;
+    private BufferedWriter magneticWriter;
+    private BufferedWriter gyroWriter;
+    //空参数构造函数，传入的值为空时，不出错
+    public FileHelper() {
+    }
+
+    public FileHelper(Context mContext,String name) throws Exception{
+        super();
+        FileOutputStream lightOutput = mContext.openFileOutput("Lighttttt"+name, Context.MODE_PRIVATE);
+        FileOutputStream gyroOutput = mContext.openFileOutput("Gyroscope"+name, Context.MODE_PRIVATE);
+        FileOutputStream magneticOutput = mContext.openFileOutput("Magnetic"+name, Context.MODE_PRIVATE);
+        FileOutputStream accelerometerOutput = mContext.openFileOutput("Accelerometer"+name, Context.MODE_PRIVATE);
+        lightWriter=new BufferedWriter(new OutputStreamWriter(lightOutput));
+        accelerometerWriter=new BufferedWriter(new OutputStreamWriter(accelerometerOutput));
+        magneticWriter=new BufferedWriter(new OutputStreamWriter(magneticOutput));
+        gyroWriter=new BufferedWriter(new OutputStreamWriter(gyroOutput));
+        this.mContext = mContext;
+    }
+
+    /*
+    * 定义文件保存的方法，写入到文件中，所以是输出流
+    * */
+    public void save(String content,final int mode) throws Exception {
+        //Context.MODE_PRIVATE权限，只有自身程序才能访问，而且写入的内容会覆盖文本内原有内容
+        switch (mode){
+            case MODE_LIGHT: {
+                lightWriter.write(content);
+                break;
+            }
+            case MODE_ACCELEROMETER: {
+                accelerometerWriter.write(content);
+                break;
+            }
+            case MODE_MAGNETIC: {
+                magneticWriter.write(content);
+                break;
+            }
+            case MODE_GYROSCOPE: {
+                gyroWriter.write(content);
+                break;
+            }
+        }
+        //将String字符串以字节流的形式写入到输出流
+    }
+    public void close() throws Exception{
+        lightWriter.close();//关闭输出流
+        gyroWriter.close();
+        accelerometerWriter.close();
+        magneticWriter.close();
+    }
+    /*
+    * 定义文件读取的方法
+    * */
+    public String read(String filename) throws IOException {
+        //打开文件输入流
+        FileInputStream input = mContext.openFileInput(filename);
+        //定义1M的缓冲区
+        byte[] temp = new byte[1024];
+        //定义字符串变量
+        StringBuilder sb = new StringBuilder("");
+        int len = 0;
+        //读取文件内容，当文件内容长度大于0时，
+        while ((len = input.read(temp)) > 0) {
+            //把字条串连接到尾部
+            sb.append(new String(temp, 0, len));
+        }
+        //关闭输入流
+        input.close();
+        //返回字符串
+        return sb.toString();
+    }
+}
---
+++ app/src/main/java/com/example/sensor/MainActivity.java
@@ -0,0 +1,180 @@
+package com.example.sensor;
+
+import android.app.Activity;
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+import com.example.sensor.FileHelper;
+import static java.lang.System.currentTimeMillis;
+
+/**
+ * Created by SHIYONG on 2017/11/3.
+ */
+
+public class MainActivity extends Activity {
+    private SensorManager sensorManager;
+    private Sensor lightSensor;
+    private Sensor accelerateSensor;
+    private Sensor magneticSensor;
+    private Sensor gyroSensor;
+
+    private TextView lightLevel;
+    private TextView gyroLevel;
+    private TextView accelerateLevel;
+    private TextView magneticLevel;
+
+    private long lastTime;
+    private int index;
+    private Button switchBtn;
+    private Button fileBtn;
+    private boolean isOpen;
+    private FileHelper fHelper;
+    private boolean isFileOpen;
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.activity_main);
+        index=0;
+        isOpen=false;
+        isFileOpen=false;
+        initView();
+        sensorManager=(SensorManager)getSystemService(Context.SENSOR_SERVICE);
+        lightSensor=sensorManager.getDefaultSensor(Sensor.TYPE_LIGHT);
+        magneticSensor=sensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD);
+        accelerateSensor=sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        gyroSensor=sensorManager.getDefaultSensor(Sensor.TYPE_GYROSCOPE);
+        sensorManager.registerListener(new MySensorEventlistener(),gyroSensor,SensorManager.SENSOR_DELAY_NORMAL);
+        sensorManager.registerListener(new MySensorEventlistener(),lightSensor,SensorManager.SENSOR_DELAY_NORMAL);
+        sensorManager.registerListener(new MySensorEventlistener(),accelerateSensor,SensorManager.SENSOR_DELAY_NORMAL);
+        sensorManager.registerListener(new MySensorEventlistener(),magneticSensor,SensorManager.SENSOR_DELAY_NORMAL);
+
+        try {
+            fHelper = new FileHelper(this,"Data.txt");
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+    @Override
+    protected void onDestroy(){
+        super.onDestroy();
+        if(sensorManager!=null){
+            sensorManager.unregisterListener(new MySensorEventlistener());
+        }
+    }
+    private void initView(){
+        switchBtn=(Button)findViewById(R.id.switchBtn);
+        fileBtn=(Button)findViewById(R.id.switchFile);
+        switchBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(isOpen){
+                    switchBtn.setText("打开传感器");
+                    isOpen=false;
+                }else {
+                    switchBtn.setText("关闭传感器");
+                    isOpen=true;
+                }
+            }
+        });
+        fileBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(isFileOpen){
+                    fileBtn.setText("打开文件读写");
+                    isFileOpen=false;
+                }else{
+                    fileBtn.setText("关闭文件读写");
+                    isFileOpen=true;
+                }
+            }
+        });
+        lightLevel=(TextView)findViewById(R.id.light_result);
+        accelerateLevel=(TextView)findViewById(R.id.accelerate_result);
+        magneticLevel=(TextView)findViewById(R.id.magnetic_result);
+        gyroLevel=(TextView)findViewById(R.id.gyroscope_result);
+    }
+    private class MySensorEventlistener implements SensorEventListener {
+        @Override
+        public void onSensorChanged(SensorEvent sensorEvent) {
+            if(!isOpen) return;
+            float []values;
+            switch (sensorEvent.sensor.getType()){
+                case Sensor.TYPE_LIGHT: {
+                    values = sensorEvent.values;
+                    lightLevel.setText("光传感器的值：" + values[0] );
+                    if(isFileOpen){
+                        try {
+                            fHelper.save("Light：" + values[0]+"\r\n",FileHelper.MODE_LIGHT);
+                        }catch (Exception e){
+                            e.printStackTrace();
+                        }
+                    }
+                    break;
+                }
+                case Sensor.TYPE_ACCELEROMETER:{
+                    /*long time=System.currentTimeMillis();
+                    long deltaTime=time-lastTime;
+                    if(index==2){
+                        Toast.makeText(MainActivity.this,""+deltaTime,Toast.LENGTH_SHORT).show();
+                    }
+                    lastTime=time;
+                    index++;*/
+                    values = sensorEvent.values;
+                    accelerateLevel.setText("X轴加速度传感器的值：" + values[0] + "\n" +
+                            "Y加速度传感器的值：" + values[1] + "\nZ轴加速度传感器的值：" + values[2]);
+                    if(isFileOpen){
+                        try {
+                            fHelper.save("X：" + values[0] +
+                                    " Y：" + values[1] + " Z：" + values[2]+"\r\n",FileHelper.MODE_ACCELEROMETER);
+                        }catch (Exception e){
+                            e.printStackTrace();
+                        }
+                    }
+                    break;
+                }
+                case Sensor.TYPE_MAGNETIC_FIELD:{
+                    values = sensorEvent.values;
+                    magneticLevel.setText("X轴地磁传感器的值：" + values[0] + "\n" +
+                            "Y地磁传感器的值：" + values[1] + "\nZ轴地磁传感器的值：" + values[2]);
+                    if(isFileOpen){
+                        try {
+                            fHelper.save("X：" + values[0] +
+                                    " Y：" + values[1] + " Z：" + values[2]+"\r\n",FileHelper.MODE_MAGNETIC);
+                        }catch (Exception e){
+                            e.printStackTrace();
+                        }
+                    }
+                    break;
+                }
+                case Sensor.TYPE_GYROSCOPE:{
+                    values = sensorEvent.values;
+                    gyroLevel.setText("X轴陀螺仪的值：" + values[0] + "\n" +
+                            "Y陀螺仪的值：" + values[1] + "\nZ轴陀螺仪的值：" + values[2]);
+                    if(isFileOpen){
+                        try {
+                            fHelper.save("X：" + values[0] +
+                                    " Y：" + values[1] + " Z：" + values[2]+"\r\n",FileHelper.MODE_GYROSCOPE);
+                        }catch (Exception e){
+                            e.printStackTrace();
+                        }
+                    }
+                    break;
+                }
+            }
+        }
+
+        @Override
+        public void onAccuracyChanged(Sensor sensor, int i) {
+
+        }
+    };
+}
